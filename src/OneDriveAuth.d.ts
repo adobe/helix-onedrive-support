14d123cc2c1c1dab8712ebf9f8a05d63217829ba
Dominique Pfister <dpfister@adobe.com>
2022-12-13 17:20:58 +0100

    chore(docs): fix type

--- src/OneDriveAuth.d.ts
+++ src/OneDriveAuth.d.ts
@@ -19,7 +19,7 @@ export declare interface OneDriveAuthOptions {
   scopes?: string[];
   onCode?: Function;
   localAuthCache?:boolean;
-  acquireMethods: string[];
+  acquireMethod?: string;
 
   /**
    * use cache plugin instead for default (global) token cache.
